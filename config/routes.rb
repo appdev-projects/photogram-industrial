40f8c60ba8308abc341f59cc0afd2a403e60be53
Jacob Hoffmann <67609232+jacobhoffmann257@users.noreply.github.com>
2023-10-23 19:27:11 +0000

    industrial 1 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root to: "photos#index"
   resources :photos
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
