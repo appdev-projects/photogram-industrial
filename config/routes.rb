f85f8c6b85101b4adb48633a76d1cdb55be34d45
Brian Kim <brianhkim13@gmail.com>
2025-01-02 21:48:06 +0000

    added tabbed interface liked feed followers and following routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
-  root 'photos#index'
-  
+  root "photos#index"
+
   devise_for :users
-  resources :likes
-  resources :follow_requests
+  
   resources :comments
+  resources :follow_requests
+  resources :likes
   resources :photos
-  get "/:username" => "users#show", as: :user
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+  get ":username/liked" => "users#liked", as: :liked
+  get ":username/feed"
+  get ":username/followers"
+  get ":username/following"
 
 
+  get ":username" => "users#show", as: :user
 end
